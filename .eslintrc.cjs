7b3f4dcc799eb4f4d72d317790037b1566e70fec
chenzhigang <979703986@qq.com>
2023-04-04 17:05:49 +0800

    chore: 格式化

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,9 +8,9 @@ module.exports = {
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
 }
