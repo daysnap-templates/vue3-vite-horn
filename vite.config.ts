f3840003414e9265b76935c438d32919d59d8c7b
chenzhigang <979703986@qq.com>
2023-04-05 11:14:10 +0800

    chore: 自动引入

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
 import DefineOptions from 'unplugin-vue-define-options/vite'
 import postCssPxToRem from 'postcss-pxtorem'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   plugins: [
     vue(),
     vueJsx(),
@@ -21,18 +27,21 @@ export default defineConfig({
       dts: 'typings/components.d.ts',
       resolvers: [VantResolver()],
     }),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: 'typings/auto-imports.d.ts',
+      resolvers: [VantResolver()],
+    }),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
   css: {
     preprocessorOptions: {
       scss: {},
     },
     postcss: {
       plugins: [
+        // https://www.npmjs.com/package/postcss-pxtorem
         postCssPxToRem({
           rootValue: 37.5, // 1rem的大小
           propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
