b0ff091b7dff522ce702bfb19de9ef4528c602a7
chenzhigang <979703986@qq.com>
2023-04-05 16:11:21 +0800

    chore: types

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,4 +1,5 @@
+import type { Userinfo } from '@/types'
 import { getLocal } from '@daysnap/utils'
 
 // 所有的数据存储到放到这里控制
-export const userinfoStorage = getLocal().generate('$$USER_INFO')
+export const userinfoStorage = getLocal().generate<Userinfo>('$$USER_INFO')
